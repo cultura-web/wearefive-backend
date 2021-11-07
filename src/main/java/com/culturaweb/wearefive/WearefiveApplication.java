89184cb76381dfcb49cf9ba4452106c253f3487b
Juan Esteban Moncada <juanestebanmb@ufps.edu.co>
2021-11-06 21:00:10 -0500

    primer template del proyecto con dependencias iniciales en maven y un controllador con endpoint /ping

---
+++ src/main/java/com/culturaweb/wearefive/WearefiveApplication.java
@@ -0,0 +1,13 @@
+package com.culturaweb.wearefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearefiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearefiveApplication.class, args);
+	}
+
+}
